a1d485ba4e7f0b335ecd7f75e88818740d0785ca
Steve <15229911+stevehanstudio@users.noreply.github.com>
2022-01-12 16:29:45 -0800

    retry changed app text

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          I was changed on the feature branch
-        </p>
-      </header>
-    </div>
-  );
+		<div className='App'>
+			<header className='App-header'>
+				<img src={logo} className='App-logo' alt='logo' />
+				<p>I was changed on the feature branch</p>
+				<a
+					className='App-link'
+					href='https://reactjs.org'
+					target='_blank'
+					rel='noopener noreferrer'
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+  )
 }
 
 export default App;
